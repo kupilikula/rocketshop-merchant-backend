7ed171da63ea2bd4808debd78807b6bffc349aed
Anand Murugan <anand.murugan@gmail.com>
2025-01-23 12:14:40 +0530

    chats

--- app.js
+++ app.js
@@ -26,8 +26,12 @@ module.exports = async function (fastify, opts) {
 
     // try {
       // const user = jwt.verify(token, JWT_SECRET); // Verify the token
-      const merchantId = request.headers.authorization;
-      request.user = {merchantId: merchantId}; // Attach the user to the request object
+      const auth = request.headers.authorization.split('=');
+      if (auth[0]==='merchantId') {
+        request.user = {merchantId: auth[1]}
+      } else if (auth[0]==='customerId') {
+        request.user = {customerId: auth[1]}
+      }
     // } catch (err) {
     //   return reply.status(401).send({ error: 'Invalid token' });
     // }
