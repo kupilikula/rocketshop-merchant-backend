e3a5b63014fa097b960ba5fbb5be8c9d71b82751
Anand Murugan <anand.murugan@gmail.com>
2025-04-15 12:35:39 +0530

    minor

---
+++ routes/stores/:storeId/activateStore/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const knex = require("@database/knexInstance");
+
+module.exports = async function (fastify, opts) {
+    fastify.patch('/', async function (request, reply) {
+        const { storeId } = request.params;
+        const requestingMerchantId = request.user.merchantId;
+        const { phone, otp } = request.body;
+
+        const latestOtp = await knex('otp_verification')
+            .where({ phone, app: 'merchant' })
+            .orderBy('created_at', 'desc')
+            .first();
+
+        if (!latestOtp || latestOtp.otp !== otp || !latestOtp.isVerified) {
+            return reply.status(401).send({ error: 'Invalid or expired OTP' });
+        }
+        // Check if requesting merchant is Admin
+        const merchant = await knex('merchantStores')
+            .where({ storeId, merchantId: requestingMerchantId })
+            .first();
+
+        if (!merchant || merchant.merchantRole !== 'Admin') {
+            return reply.status(403).send({ error: 'Only Admin merchants can activate the store.' });
+        }
+
+        await knex('stores')
+            .where({ storeId })
+            .update({
+                isActive: true,
+                updated_at: knex.fn.now(),
+            });
+
+        return reply.send({ success: true });
+    });
+};
