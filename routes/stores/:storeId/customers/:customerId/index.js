9ae83b70b9480387bbd19c26206c59796847c8bb
Anand Murugan <anand.murugan@gmail.com>
2025-04-21 19:15:22 +0530

    refactor customers

--- routes/stores/:storeId/customers/:customerId/index.js
+++ routes/stores/:storeId/customers/:customerId/index.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 const knex = require("@database/knexInstance");
+const { getCompletedOrderStatuses } = require("../../../../utils/orderStatusList");
 
 module.exports = async function (fastify, opts) {
-
   fastify.get('/', async (request, reply) => {
     const { storeId, customerId } = request.params;
 
@@ -17,16 +17,31 @@ module.exports = async function (fastify, opts) {
         return reply.status(404).send({ error: 'Customer not found.' });
       }
 
-      // Fetch order history for the store
+      // Fetch all orders for this customer at this store
       const orders = await knex('orders')
           .where({ customerId, storeId })
           .orderBy('orderDate', 'desc');
 
-      return reply.send({ ...customer, orders });
+      const completedStatuses = getCompletedOrderStatuses();
+
+      // Aggregate stats from completed orders only
+      const completedOrders = orders.filter(o => completedStatuses.includes(o.orderStatus));
+      const totalSpent = completedOrders.reduce((sum, o) => sum + Number(o.orderTotal), 0);
+      const orderCount = completedOrders.length;
+      const mostRecentOrderDate = completedOrders.length > 0
+          ? completedOrders[0].orderDate
+          : null;
+
+      return reply.send({
+        ...customer,
+        orders,
+        totalSpent,
+        orderCount,
+        mostRecentOrderDate,
+      });
     } catch (error) {
       request.log.error(error);
       return reply.status(500).send({ error: 'Failed to fetch customer details.' });
     }
   });
-
-}
+};
