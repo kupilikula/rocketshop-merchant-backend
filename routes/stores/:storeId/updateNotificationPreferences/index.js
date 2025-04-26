04c3ecca4cf6c8825108b6a1bfe0d42132afcbb8
Anand Murugan <anand.murugan@gmail.com>
2025-04-26 18:21:10 +0530

    notifs preferences

--- routes/stores/:storeId/updateNotificationPreferences/index.js
+++ routes/stores/:storeId/updateNotificationPreferences/index.js
@@ -5,7 +5,7 @@ const knex = require('@database/knexInstance');
 module.exports = async function (fastify, opts) {
     fastify.patch('/', async (request, reply) => {
         const merchantId = request.user.merchantId;
-        const { storeId } = request.body;
+        const { storeId } = request.params;
 
         if (!storeId || !merchantId) {
             return reply.status(400).send({ error: 'storeId and merchantId is required' });
