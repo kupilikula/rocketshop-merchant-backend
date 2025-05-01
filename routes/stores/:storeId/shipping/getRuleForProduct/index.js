6f58412f3b404fb180e8d14045d6a9abdb7d5732
Anand Murugan <anand.murugan@gmail.com>
2025-05-01 14:48:15 +0530

    shipping

---
+++ routes/stores/:storeId/shipping/getRuleForProduct/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const knex = require('@database/knexInstance');
+
+module.exports = async function (fastify, opts) {
+    fastify.get('/', async (request, reply) => {
+        const { storeId } = request.params;
+        const {productId} = request.query;
+
+        if (!productId) {
+            return reply.code(400).send({ error: 'Missing productId' });
+        }
+
+        const rule = await knex('shipping_rules as sr')
+            .join('product_shipping_rules as psr', 'sr.shippingRuleId', 'psr.shippingRuleId')
+            .where('psr.productId', productId)
+            .select(
+                'sr.shippingRuleId',
+                'sr.storeId',
+                'sr.ruleName',
+                'sr.conditions',
+                'sr.groupingEnabled',
+                'sr.isActive',
+                'sr.created_at',
+                'sr.updated_at'
+            )
+            .first();
+
+        if (!rule) {
+            return reply.code(404).send({ error: 'No shipping rule found for this product' });
+        }
+
+        return {
+            ...rule,
+            conditions: JSON.parse(rule.conditions),
+        };
+
+    });
+};
