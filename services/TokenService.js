d34096ece1b3a6cb31a44210791bb6bf4e97e079
Anand Murugan <anand.murugan@gmail.com>
2025-05-26 19:36:56 +0530

    token

--- services/TokenService.js
+++ services/TokenService.js
@@ -23,10 +23,7 @@ const TokenService = {
 
     verifyAccessToken(token) {
         try {
-            console.log("Verify access token, secret:", JWT_SECRET);
-            let r = jwt.verify(token, JWT_SECRET);
-            console.log("Verify access token, result:", r);
-            return r;
+            return jwt.verify(token, JWT_SECRET);
         } catch (error) {
             throw new Error('Invalid or expired token:', error);
         }
