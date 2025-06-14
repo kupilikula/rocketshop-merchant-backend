826c606c2093b0ade265f3d3bbe7401a19d61c20
Anand Murugan <anand.murugan@gmail.com>
2025-06-14 22:10:58 +0530

    rename qa branch

--- knexfile.js
+++ knexfile.js
@@ -24,4 +24,18 @@ module.exports = {
       directory: './database/seeds',
     }
   },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+  },
 };
