6c430e841dc1844f199fc1bc670a92ebbdeec98d
Anand Murugan <anand.murugan@gmail.com>
2025-06-17 23:01:27 +0530

    fix bug

--- routes/razorpay/exchangeCodeForTokens/index.js
+++ routes/razorpay/exchangeCodeForTokens/index.js
@@ -69,7 +69,20 @@ async function performRouteSetup(logger, affiliateAccountId, storeId) {
             const routeAccountPayload = {
                 email: storeProfile.storeEmail, phone: storeProfile.storePhone, legal_business_name: storeProfile.legalBusinessName,
                 customer_facing_business_name: storeProfile.storeName, type: "route", business_type: storeProfile.businessType,
-                profile: { category: storeProfile.category, subcategory: storeProfile.subcategory, addresses: { registered: { street1: storeProfile.registeredAddress.street1, street2: storeProfile.registeredAddress.street2, city: storeProfile.registeredAddress.city, state: storeProfile.registeredAddress.state, country: "IN", postal_code: storeProfile.registeredAddress.postalCode }}}
+                profile: {
+                    category: storeProfile.category,
+                    subcategory: storeProfile.subcategory,
+                    addresses: {
+                        registered: {
+                            street1: storeProfile.registeredAddress.street1,
+                            street2: storeProfile.registeredAddress.street2 || '',
+                            city: storeProfile.registeredAddress.city,
+                            state: storeProfile.registeredAddress.state,
+                            country: "IN",
+                            postal_code: storeProfile.registeredAddress.postalCode
+                        }
+                    }
+                }
             };
             const routeAccountResponse = await axios.post('https://api.razorpay.com/v2/accounts', routeAccountPayload, { headers });
             newRazorpayRouteAccountId = routeAccountResponse.data.id;
